8e07ae560504710f4ae4e854eed6c74b18c62558
AsmitaHari <50839635+AsmitaHari@users.noreply.github.com>
2021-04-09 15:20:28 -0400

    typeScript]

---
+++ features/annotations/variables.ts
@@ -0,0 +1,12 @@
+const apples: number = 9;
+
+let colors: string[] = ['red','g','b']
+
+let point: {x:number,y:number} = {
+    x:9,
+    y:90
+}
+
+const func: (i:number)=> void = (i:number)=>{
+    
+}
